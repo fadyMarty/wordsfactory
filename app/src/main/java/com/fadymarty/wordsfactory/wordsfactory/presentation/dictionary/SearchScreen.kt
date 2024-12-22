7e72f3f8d15cb3a2a2f6a4819dc20cd0627468e0
fadyMarty <antoshka1234098@gmail.com>
2024-12-23 00:51:54 +0300

    Commit

--- app/src/main/java/com/fadymarty/wordsfactory/wordsfactory/presentation/dictionary/SearchScreen.kt
+++ app/src/main/java/com/fadymarty/wordsfactory/wordsfactory/presentation/dictionary/SearchScreen.kt
@@ -1,13 +1,13 @@
 package com.fadymarty.wordsfactory.presentation.dictionary
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.fadymarty.wordsfactory.domain.model.Word
 import com.fadymarty.wordsfactory.presentation.common.SearchBar
 import com.fadymarty.wordsfactory.presentation.common.WordsButton
 
@@ -29,32 +29,30 @@ fun DictionaryScreen(
             onValueChange = viewModel::onValueChange,
             onSearch = viewModel::onSearch
         )
-        Column(
-            modifier = Modifier
-                .padding(
-                    start = 16.dp,
-                    top = 16.dp,
-                    end = 16.dp
-                )
+        LazyColumn(
+            Modifier.weight(1f),
+            contentPadding = PaddingValues(
+                start = 16.dp,
+                top = 16.dp,
+                end = 16.dp
+            )
         ) {
-            if (wordState.wordItems != emptyList<Word>()) {
-
-                val word = wordState.wordItems[0]
+            items(wordState.wordItems.size) { i ->
+                val word = wordState.wordItems[i]
                 WordInfo(word)
-
-                Spacer(modifier = Modifier.weight(1f))
-
-                WordsButton(
-                    modifier = Modifier.padding(
-                        start = 24.dp,
-                        end = 24.dp,
-                        bottom = 8.dp
-                    ),
-                    text = "Add to Dictionary",
-                    onClick = { }
-                )
             }
         }
+
         DictionaryScreen(wordState.isDictionaryEmpty)
+
+        WordsButton(
+            modifier = Modifier.padding(
+                start = 24.dp,
+                end = 24.dp,
+                bottom = 8.dp
+            ),
+            text = "Add to Dictionary",
+            onClick = { }
+        )
     }
 }
--- app/src/main/java/com/fadymarty/wordsfactory/wordsfactory/presentation/dictionary/WordInfo.kt
+++ app/src/main/java/com/fadymarty/wordsfactory/wordsfactory/presentation/dictionary/WordInfo.kt
@@ -1,21 +1,15 @@
 package com.fadymarty.wordsfactory.presentation.dictionary
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.fadymarty.ui.theme.Rubik
@@ -24,7 +18,7 @@ import com.fadymarty.wordsfactory.presentation.common.WordCard
 
 @Composable
 fun WordInfo(
-    word: Word
+    word: Word,
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically
@@ -36,11 +30,11 @@ fun WordInfo(
             fontSize = 24.sp
         )
 
-        word.phonetic?.let {
+        word.phonetic?.let { phonetic ->
             Spacer(modifier = Modifier.width(16.dp))
 
             Text(
-                text = "[${it.drop(1).dropLast(1)}]",
+                text = "[${phonetic.drop(1).dropLast(1)}]",
                 fontFamily = Rubik,
                 fontSize = 14.sp,
                 color = MaterialTheme.colorScheme.primary
@@ -50,26 +44,25 @@ fun WordInfo(
 
     Spacer(modifier = Modifier.height(16.dp))
 
-    Row(
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = "Part of Speech:",
-            fontFamily = Rubik,
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp
-        )
+    word.meanings.forEach { meaning ->
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Part of Speech:",
+                fontFamily = Rubik,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
 
-        Spacer(modifier = Modifier.width(16.dp))
+            Spacer(modifier = Modifier.width(16.dp))
 
-        Text(
-            text = word.meanings[0].partOfSpeech.replaceFirstChar(Char::titlecase),
-            fontFamily = Rubik,
-            fontSize = 14.sp
-        )
-    }
-
-    if (word.meanings[0].definitions.size > 1) {
+            Text(
+                text = meaning.partOfSpeech.replaceFirstChar(Char::titlecase),
+                fontFamily = Rubik,
+                fontSize = 14.sp
+            )
+        }
 
         Spacer(modifier = Modifier.height(16.dp))
 
@@ -82,14 +75,19 @@ fun WordInfo(
 
         Spacer(modifier = Modifier.height(11.dp))
 
-        WordCard(
-            definition = word.meanings[0].definitions[0]
-        )
+        meaning.definitions.forEachIndexed { index, definition ->
+            if (index == meaning.definitions.lastIndex) {
+                WordCard(
+                    definition = definition
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+            } else {
+                WordCard(
+                    definition = definition
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
 
-        Spacer(modifier = Modifier.height(8.dp))
-
-        WordCard(
-            definition = word.meanings[0].definitions[1]
-        )
+        }
     }
 }
